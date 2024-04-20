638ba2cd4a5cbe2c374735ea2740387dfee656fd
kristopher.iotov <kristopher.iotov@gmail.com>
2024-04-20 14:25:33 -0700

    Adding HTML templates

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('InterestsApp.urls')),  # Replace 'your_app_name' with the name of your app
 ]
