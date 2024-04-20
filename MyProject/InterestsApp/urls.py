3334d2b45c0d3d9f189541a2d4b89a398b6bf90b
kristopher.iotov <kristopher.iotov@gmail.com>
2024-04-20 14:45:52 -0700

    Adding api.html template to views and urls.py

--- MyProject/InterestsApp/urls.py
+++ MyProject/InterestsApp/urls.py
@@ -1,6 +1,9 @@
+# urls.py in InterestsApp
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/', views.api_view, name='api'),
 ]
